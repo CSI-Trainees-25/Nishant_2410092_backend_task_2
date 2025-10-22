6628a2143591ceb76da5eb450650a9ec2e455185
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-10-22 23:06:43 +0530

    connected database

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ import dotenv from 'dotenv'
  dotenv.config({
     path: '.env'
 })
+import connectDB from './Src/db/index.js'
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 7000, () => {
+            console.log(`server is ready at ${process.env.PORT}`);
+        });
+        app.on("error", (error) => {
+            console.error("ERRORR:", error)
+            throw error
+        })
+    })
+    .catch((error) => {
+        console.log("mongo DB connection failed", error);
+    })
+
+
 app.get('/api',(req,res) => {
    res.send('hello task_2  Authentication System ')
-}
-
-)
-app.listen(process.env.PORT || 7000,() => {
-   console.log(`server is ready at ${process.env.PORT}`)
 }
 )
